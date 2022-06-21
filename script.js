64f7cdc3a924abf7240d300903276dd93913fe71
WALEED SHAHID <75040621+waleedahmed0001@users.noreply.github.com>
2022-06-21 11:00:14 +0500

    Ecommerce FIles

---
+++ script.js
@@ -0,0 +1,23 @@
+const bar=document.getElementById('bar');
+const nav=document.getElementById('navbar');
+const close=document.getElementById('close');
+
+
+
+if (bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+
+        // bar.classList.add('active')
+        // console.log(nav)
+    })
+}
+
+if (close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+
+        // bar.classList.add('active')
+        // console.log(nav)
+    })
+}
